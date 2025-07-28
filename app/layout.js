0243ea46a1c9c7067a8493f039c6174997dd4d68
zeesee123 <zubinchadha@gmail.com>
2025-07-28 17:31:41 +0530

    google tag

--- app/layout.js
+++ app/layout.js
@@ -41,6 +41,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+
+        <script
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T3HV4QT9');`,
+  }}
+/>
+
+
         {/* Tailwind CDN */}
         <script src="https://cdn.tailwindcss.com"></script>
 
@@ -68,10 +80,20 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Lustria&family=Raleway:wght@400;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+
+        
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${raleway.variable} ${lustria.variable} ${playfair.variable} antialiased`}
       >
+
+     <noscript
+  dangerouslySetInnerHTML={{
+    __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3HV4QT9"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+  }}
+></noscript>
+
         <Navbar />
         {children}
         <Footer />
