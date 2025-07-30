8e28f3f0054a730651919933b29f0162d61a10f7
zeesee123 <zubinchadha@gmail.com>
2025-07-30 16:54:37 +0530

    meta title and desc

--- app/page.js
+++ app/page.js
@@ -1,32 +1,10 @@
-"use client";
-import React from "react";
-import {
-  CleansingRange,
-  HandcareSec,
-  SkincareSection,
-  YourSkinStory,
-} from "./components/home/Skin";
-import { HeroSection, FacialMaskSec } from "./components/home/HeroSec";
-import {
-  EssenceAndBalmSection,
-  EyeCare,
-  PatchSection,
-} from "./components/home/BamHydrasec";
-import { Mask } from "./components/home/Mask";
+export const metadata = {
+  title: "PR Activity",
+  description: "PR Activity",
+};
 
-export default function Home() {
-  return (
-    <>
-      <HeroSection />
-      <YourSkinStory />
-      <Mask />
-      <FacialMaskSec />
-      <SkincareSection />
-      <CleansingRange />
-      <HandcareSec />
-      <EssenceAndBalmSection />
-      <PatchSection />
-      <EyeCare />
-    </>
-  );
+import HomeClient from './HomeClient';
+
+export default function ServerPage() {
+  return <HomeClient />;
 }
